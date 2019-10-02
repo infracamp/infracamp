fca3451e642cc313f8de47ca218b83111b19abed
Matthes <matthes@leuffen.de>
2019-10-03 01:32:02 +0200

    wurst

--- pushback.sh
+++ pushback.sh
@@ -7,11 +7,11 @@ chmod 700 ~/.ssh/id_rsa
 
 git config --global user.email "you@example.com"
 git config --global user.name "CI Builder"
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git clone git@github.com:infracamp/infracamp.git
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git clone git@github.com:infracamp/infracamp.git
 cd infracamp
 echo "wurst2" > wurst.txt
 git add .
-git commit -m "autobuild"
+git commit -m "[skipci] autobuild"
 
 if [ "$?" != "0" ]
 then
@@ -19,4 +19,4 @@ then
   exit 0;
 fi;
 
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git push
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git push
