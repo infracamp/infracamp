8d181050410c7f8a32ebc1d43e8734a06efdbe72
Matthes <matthes@leuffen.de>
2019-10-03 01:27:51 +0200

    wurst

--- pushback.sh
+++ pushback.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-set -e -x
+set -e
 
 mkdir -p ~/.ssh
 echo "$SSH_PRIV_KEY" > ~/.ssh/id_rsa
 chmod 700 ~/.ssh/id_rsa
 
 git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git clone git@github.com:infracamp/infracamp.git
+git config --global user.name "CI Builder"
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git clone git@github.com:infracamp/infracamp.git
 cd infracamp
-echo "wurst" > wurst.txt
+echo "wurst2" > wurst.txt
 git add .
 git commit -m "autobuild"
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git push
+
+if [ "$?" != "0" ]
+then
+  echo "no changes"
+  exit 0;
+fi;
+
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" git push
