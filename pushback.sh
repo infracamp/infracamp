6345ea83d084a5502439e93df684dab8def6e798
Matthes <matthes@leuffen.de>
2019-10-03 01:42:35 +0200

    added redis page

--- pushback.sh
+++ pushback.sh
@@ -9,18 +9,16 @@ git config --global user.email "you@example.com"
 git config --global user.name "CI Builder"
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git clone git@github.com:infracamp/infracamp.git
 cd infracamp
-echo "wurst2" > wurst.txt
+
+
+## changes to repository
+git submodule update --remote
+
+curl -o repos/redis.md https://raw.githubusercontent.com/infracamp/redis/master/README.md
 
 
 
 
 git add .
 git commit -m "[skipci] autobuild" || true
-
-if [ "$?" != "0" ]
-then
-  echo "no changes"
-  exit 0;
-fi;
-
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" git push
